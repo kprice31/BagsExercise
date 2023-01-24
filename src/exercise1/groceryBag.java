7f3210d48fa7a9c23c9d673914f1e60629662612
kprice31 <54342102+kprice31@users.noreply.github.com>
2023-01-23 19:25:46 -0700

    Initial commit

---
+++ src/exercise1/groceryBag.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class groceryBag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String itemOne = "Broccoli";
+		
+	}
+	
+	
+
+}
