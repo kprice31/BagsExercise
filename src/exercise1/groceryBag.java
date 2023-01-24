276ff368ba504a38d8658859775f77caa1da85b3
kprice31 <54342102+kprice31@users.noreply.github.com>
2023-01-23 19:27:31 -0700

    Created two strings of grocery item lists

--- src/exercise1/groceryBag.java
+++ src/exercise1/groceryBag.java
@@ -5,8 +5,10 @@ public class groceryBag {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String itemOne = "Broccoli";
-		
+		String itemOne = "Broccoli"; 
+		String itemTwo = "Spaghetti";
+		System.out.println(itemOne);
+		System.out.println(itemTwo);
 	}
 	
 	
